82f88b78f6fb32c289c14b06b32fe07ca1702df2
twinitiee <twinitiee@gmail.com>
2018-07-31 17:13:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "~bops by trinity~"
+  end
+  
+  get '/name' do
+    "my name is trinity"
+  end
+  
+  get '/hometown' do
+    "my hometown is allentown pennsylvania"
+  end
+  
+  get '/favorite-song' do
+    "my favorite song is anything from my bops by trinity list"
+  end
 end
